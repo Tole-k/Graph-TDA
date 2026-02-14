4068a91aba217e15c256d87b9c2794469d5a6a50
Anatol Kaczmarek <92335424+Tole-k@users.noreply.github.com>
2026-02-14 13:35:46 +0100

    add main function

--- Graph-TDA/main.py
+++ Graph-TDA/main.py
@@ -6,9 +6,10 @@ from autoirad_datasets import generate_training_parameters
 import torch
 from sklearn.model_selection import train_test_split
 
-epsilons = [0.25, 1, 5, 25]
+def main():
+    epsilons = [0.25, 1, 5, 25]
 
-device = torch.device(
+    device = torch.device(
     "cuda"
     if torch.cuda.is_available()
     else "mps"
@@ -16,50 +17,53 @@ device = torch.device(
     else "cpu"
 )
 
-datasets, scores = generate_training_parameters()
-train_datasets, val_datasets, train_scores, val_scores = train_test_split(
+    datasets, scores = generate_training_parameters()
+    train_datasets, val_datasets, train_scores, val_scores = train_test_split(
     datasets, scores, test_size=0.2, random_state=42
 )
-train_dataloader = get_dataloader(train_datasets, train_scores, epsilons=epsilons)
+    train_dataloader = get_dataloader(train_datasets, train_scores, epsilons=epsilons)
 
-test_loader = get_dataloader(val_datasets, val_scores, epsilons=epsilons, shuffle=False)
+    test_loader = get_dataloader(val_datasets, val_scores, epsilons=epsilons, shuffle=False)
 
-model = GraphSequenceModel(in_channels=1, hidden_dim=256, out_dim=17).to(device)
-optimizer = torch.optim.AdamW(model.parameters())
-criterion = nn.MSELoss()
+    model = GraphSequenceModel(in_channels=1, hidden_dim=256, out_dim=17).to(device)
+    optimizer = torch.optim.AdamW(model.parameters())
+    criterion = nn.MSELoss()
 
-epochs = 10000
-best_loss = float("inf")
-early_stopping_patience = 10
+    epochs = 10000
+    best_loss = float("inf")
+    early_stopping_patience = 10
 
-for epoch in range(epochs):
-    model.train()
+    for epoch in range(epochs):
+        model.train()
 
-    losses = []
-    for batch in train_dataloader:
-        optimizer.zero_grad()
-        X, scores = batch
-        outputs = model(X)
-        loss = criterion(outputs, scores.to(device))
-        loss.backward()
-        optimizer.step()
-
-    for batch in test_loader:
-        model.eval()
-        with torch.inference_mode():
+        losses = []
+        for batch in train_dataloader:
+            optimizer.zero_grad()
             X, scores = batch
             outputs = model(X)
             loss = criterion(outputs, scores.to(device))
-            losses.append(loss.item())
+            loss.backward()
+            optimizer.step()
 
-    mean_loss = np.mean(losses)
-    if mean_loss < best_loss:
-        best_loss = mean_loss
-        early_stopping_patience = 10
-    else:
-        early_stopping_patience -= 1
-        if early_stopping_patience == 0:
-            print(f"Early stopping at epoch {epoch + 1}")
-            break
+        for batch in test_loader:
+            model.eval()
+            with torch.inference_mode():
+                X, scores = batch
+                outputs = model(X)
+                loss = criterion(outputs, scores.to(device))
+                losses.append(loss.item())
 
-    print(f"Epoch {epoch + 1} | Loss: {np.mean(losses):.4f}")
+        mean_loss = np.mean(losses)
+        if mean_loss < best_loss:
+            best_loss = mean_loss
+            early_stopping_patience = 10
+        else:
+            early_stopping_patience -= 1
+            if early_stopping_patience == 0:
+                print(f"Early stopping at epoch {epoch + 1}")
+                break
+
+        print(f"Epoch {epoch + 1} | Loss: {np.mean(losses):.4f}")
+
+if __name__ == "__main__":
+    main()
